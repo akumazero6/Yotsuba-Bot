ea94a139a99b22d3e8e238f4360a83d897819c46
animefutaba <102878540+animefutaba@users.noreply.github.com>
2022-04-04 12:02:12 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '🏴‍☠️ *Moderators* 🏴‍☠️\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n⚡ *Username: ${
+                (text += `#${index + 1}\n🏴‍☠️ *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n⚡ Yotsuba ⚡ `
+        text += `\n🏴‍☠️ luffy 🏴‍☠️ `
         return void M.reply(text)
     }
 }
